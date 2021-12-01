b31ec68e5eff9ef46b887250b5b0514a8c76cd4d
Edwike12 <nyaunchoedwike@gmail.com>
2021-12-02 00:23:51 +0300

    created a profile info function and imported profileservices and httpmodule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
+import {ProfileServices} from '/profile.services';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +17,7 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileServices],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -6,9 +6,18 @@ import  {map} from 'rxjs/operators/';
   providedIn: 'root'
 })
 export class ProfileService {
-  private username: string;
+  private  username= 'string';
   private clientid= 'f8abf511a4cc8a44de6c';
   private clientsecret='70a87a21221fead4884624f8fafe9e478d555686';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    console.log("services is now ready");
+    this.username = 'Edwike12';
+   }
+
+   getProfileInfo (){
+    return this.http.get("https://api.github.com/users/"+ this.username + "?client_id="+ this.clientid +"&client_secret=" +this.clientsecret)
+    .pipe(
+    map(res=> { return res.json()}));
+  }
 }
