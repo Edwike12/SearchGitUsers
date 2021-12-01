d06d09045e4f2b817db00feeb8d6b75e2cd6d0f4
Edwike12 <nyaunchoedwike@gmail.com>
2021-12-01 23:58:16 +0300

    created properties for services

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -6,6 +6,9 @@ import  {map} from 'rxjs/operators/';
   providedIn: 'root'
 })
 export class ProfileService {
+  private username: string;
+  private clientid= 'f8abf511a4cc8a44de6c';
+  private clientsecret='70a87a21221fead4884624f8fafe9e478d555686';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
